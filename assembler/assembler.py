e31e6018abaa4e7c0e9ef0141923f117594ce7f7
guishas <lunettagui@gmail.com>
2022-10-22 16:57:09 -0300

    arquivando projeto

--- assembler/assembler.py
+++ assembler/assembler.py
@@ -1,21 +1,27 @@
+# Arquivos de entrada e saída do assembler
 entrada = 'entrada.txt'
 saida = 'assembly.txt'
 
+# Dicionário para guardar as labels e suas respectivas posições e tipos
 labels = {}
 
+# Função para converter endereços, por exemplo @352, para binário
 def converteArroba(line):
   items = line.replace("\n", "").split('@')
   addr = bin(int(items[-1]))[2:].zfill(9)
   return addr
 
+# Função para converter números, por exemplo $15, para binário
 def converteCifrao(line):
   items = line.replace("\n", "").split('$')
   num = bin(int(items[-1]))[2:].zfill(9)
   return num
 
+# Função para pegar o mnemônico de uma linha
 def converteMnemonico(line):
   return line.replace("\n", "").split(' ')[0]
 
+# Função para converter o que vem do arquivo de entrada para código assembly
 def converteParaLabel(line):
   if '@' in line:
     l = converteMnemonico(line) + ' & \"' + converteArroba(line) + '\";'
@@ -28,15 +34,19 @@ def converteParaLabel(line):
 
   return l 
 
+# Função para retornar a posição da memória de instruções de uma linha
 def pegaPosicaoTmp(line):
   return line.split('(')[0][:1]
 
+# Função para retornar uma label a partir de um JMP, JEQ, JGT ou JSR
 def converteLabel(line):
   return line.split('%')[-1].replace("\n", "")
 
+# Abrindo arquivo de entrada, lendo as linhas e salvando em uma variável
 with open(entrada, "r") as f:
   lines = f.readlines()
 
+# Código para calcular em que posição cada LABEL vai estar
 cont_labels = 0
 for index, line in enumerate(lines):
   line = line.replace("\n", "")
@@ -44,6 +54,7 @@ for index, line in enumerate(lines):
     labels[line.split(':')[0]] = {"pos": index-cont_labels}
     cont_labels+=1
 
+# Código para colocar tipos de JUMP nos JUMPs do código
 for index, line in enumerate(lines):
   line = line.replace("\n", "")
   if 'JSR' in line:
@@ -55,6 +66,7 @@ for index, line in enumerate(lines):
       type = line.split('%')[0]
       labels[label]["type"] = type.replace(" ", "")
 
+# Código para de fato escrever no arquivo de saída o código assembly usando todas as funções acima como ajuda
 with open(saida, "w+") as f:
 
   cont = 0
